0135f50180f4300ab5e07452b3775623691f350f
Marcelo-Kenji-Noda <54538758+Marcelo-Kenji-Noda@users.noreply.github.com>
2021-08-04 10:43:33 -0300

    First Commitment

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md","r") as fh:
     long_description=fh.read()
 
 setup(
-    name='helloworld',
+    name='PandaTex',
     version='0.0.1',
     description='Say hello!',
     py_modules= ["helloworld"],
